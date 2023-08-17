536cd60e4667db92387e20489019b23056db0fb6
TheBiggestTrees <141491446+TheBiggestTrees@users.noreply.github.com>
2023-08-16 19:48:06 -0500

    Create grid and hover draw function
    Turns squares black when hovered over.

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-const main = document.querySelector('main');
+let div = document.querySelector('#main');
+
+function createGrid() {
+let grid = document.createElement('div');
+div.appendChild(grid);
+
+grid.className = "grid";
+
+grid.addEventListener('mouseover', (e) => {
+    e.target.style.backgroundColor = 'black';
+});
+
+return grid;
+}
+
+
+
+for (i = 256; i >= 1; i--) {
+    createGrid();
+}
+
+
 
