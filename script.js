fe0e214b10181044db7fde64d1b30af7d01b5aa4
TheBiggestTrees <141491446+TheBiggestTrees@users.noreply.github.com>
2023-08-16 20:48:14 -0500

    Add custom sizing for grid
    Grid now moves according to user's input on number of boxes per side.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let div = document.querySelector('#main');
 
+
+
+
+
 function createGrid() {
     
     let grid = document.createElement('div');
@@ -15,11 +19,22 @@ function createGrid() {
     return grid;
 }
 
+function getSides() {
+    let sides = prompt("Type in a number to make the grid. Cannot be over 100");
+    let width = (24*sides) + 'px';
+    let height = (24*sides) + 'px'
+
+    
 
 
-for (i = 256; i >= 1; i--) {
-    createGrid();
+
+
+    if (sides < 100){
+        for (i = sides**2; i >= 1; i--) {
+                div.setAttribute('style', `width: ${width}; height: ${height};`);    
+                createGrid();  
+            }
+
+    }
+
 }
-
-
-
