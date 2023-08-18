a51606c96bc2d6f2a8492016db255c462b1fefb8
TheBiggestTrees <treeleaves229@gmail.com>
2023-08-18 14:55:54 -0500

    Rows added
    created rows in css file to flex inside main.
    Allows perfect square grid in one sized box.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let div = document.querySelector('#main');
 
-function createGrid() {
-    let grid = document.createElement('div');
-    
-    div.appendChild(grid);
+function createGrid(rows, lW) {
 
+    let grid = document.createElement('div');
+    rows.appendChild(grid);
     grid.className = "grid";
-    
+    grid.setAttribute('style', `width: ${lW}`)
+
     grid.addEventListener('mouseover', (e) => {
         randomColor = Math.floor(Math.random()*16777215).toString(16);
         e.target.style.backgroundColor = '#' + randomColor;
@@ -15,16 +15,21 @@ function createGrid() {
 }
 
 function getSides() {
-
-    let sides = prompt("Type in a number to make the grid. Cannot be over 100");
-
-    
+    let sides = +prompt("Type in a number to make the grid. Cannot be over 100");
 
     if (sides < 100){
-        for (i = sides**2; i >= 1; i--) {
-                createGrid();  
-            }
-    
+        for (i = sides; i >= 1; i--) {
+
+            let rows = document.createElement('div');
+            let lW = 750 / sides + 'px'
+            div.appendChild(rows);
+            rows.className = "rows";
+            rows.setAttribute('style', `height: ${lW};`)
+
+                for (x = 0; x < sides; x++) {
+                    createGrid(rows, lW);  
+                }
+            }   
     }
     
 }
