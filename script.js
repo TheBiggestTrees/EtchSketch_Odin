277d169bd9c84afb49205fdce2aa4efef83ac270
TheBiggestTrees <treeleaves229@gmail.com>
2023-08-18 12:32:06 -0500

    Solidify size of box
    Must fix row and column bugx

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 let div = document.querySelector('#main');
 
-
-
-
-
 function createGrid() {
-    
     let grid = document.createElement('div');
+    
     div.appendChild(grid);
 
     grid.className = "grid";
-
+    
     grid.addEventListener('mouseover', (e) => {
         randomColor = Math.floor(Math.random()*16777215).toString(16);
         e.target.style.backgroundColor = '#' + randomColor;
@@ -20,21 +16,16 @@ function createGrid() {
 }
 
 function getSides() {
+
     let sides = prompt("Type in a number to make the grid. Cannot be over 100");
-    let width = (24*sides) + 'px';
-    let height = (24*sides) + 'px'
 
     
 
-
-
-
     if (sides < 100){
         for (i = sides**2; i >= 1; i--) {
-                div.setAttribute('style', `width: ${width}; height: ${height};`);    
                 createGrid();  
             }
-
+    
     }
-
+    
 }
